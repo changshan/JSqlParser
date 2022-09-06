b1a14b119e9ca1d25ea476db1ea16101e280dac6
Changshan@Work <lichangshan@xiaomi.com>
2022-09-07 01:00:06 +0800

    修改对database关键字的支持

--- src/main/java/com/xiaomi/smartql/parser/CCJSqlParserManager.java
+++ src/main/java/com/xiaomi/smartql/parser/CCJSqlParserManager.java
@@ -1,12 +1,3 @@
-/*-
- * #%L
- * JSQLParser library
- * %%
- * Copyright (C) 2004 - 2019 JSQLParser
- * %%
- * Dual licensed under GNU LGPL 2.1 or Apache License 2.0
- * #L%
- */
 package com.xiaomi.smartql.parser;
 
 import net.sf.jsqlparser.JSQLParserException;
