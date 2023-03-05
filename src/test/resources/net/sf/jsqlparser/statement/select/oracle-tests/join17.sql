2846f399b39ff0696c458d2e6785f5573c2e8892
Changshan@Work <lichangshan@xiaomi.com>
2023-03-05 14:23:31 +0800

    remove print key

--- src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/join17.sql
+++ src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/join17.sql
@@ -15,3 +15,4 @@ on c.id = s.id
 on a.va = s.va
 
 --@SUCCESSFULLY_PARSED_AND_DEPARSED first on Aug 14, 2021 9:00:57 PM
+--@FAILURE: select*from hdr a inner join sh s inner join ca c on c.id=s.id and a.va=s.va recorded first on Mar 5, 2023 1:44:56 PM
